356b72523d5870901b05d37508f961504c67e66f
Flavio Aguilar <flavio.red0568@gmail.com>
2026-03-28 10:19:43 -0600

    dosc(HellorWorld.java): add docstring for helloworld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
+/**
+ * HelloWorld.java
+ * A simple Java program that displays "Hello, World!" in a dialog box.
+ */
 import javax.swing.JOptionPane;
 public class HelloWorld {
+    /**
+    *Main method that serves as the entry point of the program. It uses JOptionPane to display a dialog box with the message "Hello, World!".
+    */
     public static void main(String[] args){
         JOptionPane.showMessageDialog(null, "Hello, World!");
     }
